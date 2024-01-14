2f9cc86a062429451c12dfae477bc20a1d353125
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2024-01-13 17:45:17 -0800

    Added function to prevent the creation of multiple accounts under the same email address

--- application/server/controller/user-controller.js
+++ application/server/controller/user-controller.js
@@ -5,24 +5,31 @@ const {
 
 module.exports = {
   newUser: (req, res) => {
-    const { username, email, password, rePassword } = req.body;
+    const { username, email, password } = req.body;
 
-    bcrypt
+    User.findOne({ where: { email }})
+    .then((user) => {
+      if (user) {
+        res.sendStatus(409);
+      } else {
+        bcrypt
       .genSalt(10)
-      .then((salt) => bcrypt.hash(password, salt))
-      .then((hashedPassword) => {
-        return User.create({
-          username,
-          email,
-          password: hashedPassword,
-        });
-      })
-      .then(() => {
-        res.sendStatus(201);
-      })
-      .catch((err) => {
-        res.status(500).send(err.message);
-        console.log(err);
-      })
+        .then((salt) => bcrypt.hash(password, salt))
+        .then((hashedPassword) => {
+          return User.create({
+            username,
+            email,
+            password: hashedPassword,
+          });
+        })
+        .then(() => {
+          res.sendStatus(201);
+        })
+        .catch((err) => {
+          res.status(500).send(err.message);
+          console.log(err);
+        })
+      }
+    });
   }
 }
