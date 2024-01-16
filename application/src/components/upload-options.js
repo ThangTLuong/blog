773aaf1c6c2ee018dbcdea966499e9a6609081c9
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2024-01-16 13:23:57 -0800

    Added flex to upload page; Starting protectedRoutes

--- application/src/components/upload-options.js
+++ application/src/components/upload-options.js
@@ -5,17 +5,15 @@ import uploadOptionsMain from "../resources/options.png"
 function UploadOptions() {
 
   return (
-    <div>
-      <div className="upload-options-group">
-        <div className="dropdown-toggle">
-          <img id="dropdown-toggle-icon" src={uploadOptionsMain} alt="Base button for upload option" />
-          <ul className="upload-dropdown-menu">
-            <li className="upload-dropdown-item"><a href="/"><img className="dropdown-item-icon" src={uploadOptionsMain} alt="Dropdown item 1" /></a></li>
-            <li className="upload-dropdown-item"><a href="/"><img className="dropdown-item-icon" src={uploadOptionsMain} alt="Dropdown item 2" /></a></li>
-            <li className="upload-dropdown-item"><a href="/"><img className="dropdown-item-icon" src={uploadOptionsMain} alt="Dropdown item 3" /></a></li>
-            <li className="upload-dropdown-item"><a href="/"><img className="dropdown-item-icon" src={uploadOptionsMain} alt="Dropdown item 4" /></a></li>
-          </ul>
-        </div>
+    <div className="upload-options-group">
+      <div className="dropdown-toggle">
+        <img id="dropdown-toggle-icon" src={uploadOptionsMain} alt="Base button for upload option" />
+        <ul className="upload-dropdown-menu">
+          <li className="upload-dropdown-item"><a href="/"><img className="dropdown-item-icon" src={uploadOptionsMain} alt="Dropdown item 1" /></a></li>
+          <li className="upload-dropdown-item"><a href="/"><img className="dropdown-item-icon" src={uploadOptionsMain} alt="Dropdown item 2" /></a></li>
+          <li className="upload-dropdown-item"><a href="/"><img className="dropdown-item-icon" src={uploadOptionsMain} alt="Dropdown item 3" /></a></li>
+          <li className="upload-dropdown-item"><a href="/"><img className="dropdown-item-icon" src={uploadOptionsMain} alt="Dropdown item 4" /></a></li>
+        </ul>
       </div>
     </div>
   );
--- application/src/pages/upload.js
+++ application/src/pages/upload.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 import UploadOptions from "../components/upload-options";
+import LeftOptions from "../components/upload-left-options";
 
 function Upload() {
   return (
-    <div>
+    <div id="body">
+      <div className="flex h-full">
+        <div className="w-1/4">
+          <LeftOptions />
+        </div>
+        <div className="w-1/2"></div>
+        <div className="w-1/4"></div>
+      </div>
       <UploadOptions />
     </div>
   );
