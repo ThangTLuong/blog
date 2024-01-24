0a73d09d5f587b201f8f31ff2975531aa053fe6f
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2024-01-23 16:58:48 -0800

    Refactored the three sections for general use

--- application/src/pages/upload.js
+++ application/src/pages/upload.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import Tooltip from "../components/tooltip";
 
 import UploadOptions from "../components/upload/upload-options";
-import LeftSection from "../components/upload/left-section";
-import RightSection from "../components/upload/right-section";
-import MainSection from "../components/upload/main-section";
+import LeftSection from "../components/left-section";
+import RightSection from "../components/right-section";
+import MainSection from "../components/main-section";
+
+import options from "../resources/options.png";
 
 import { PostContainer, PostMetadata, Post, PostText, PostMedia, PostStats } from "../components/upload/base-post";
 
@@ -75,9 +78,144 @@ const Upload = () => {
   return (
     <div id="body">
       <div className="display-sections-group flex h-full w-full">
-        <LeftSection onAddPostText={onAddPostText} onAddPostMedia={onAddPostMedia} onPostClear={onPostClear}/>
-        <MainSection addElements={addElements} />
-        <RightSection />
+        <LeftSection>
+          <div className="w-full p-2">
+            <Tooltip text={ "Text" } direction={"left"}>
+              <div className="bg-black rounded-full" onClick={onAddPostText}>
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </div>
+            </Tooltip>
+          </div>
+          <div className="w-full p-2">
+            <Tooltip text={ "Media" } direction={"left"}>
+              <div className="bg-black rounded-full" onClick={onAddPostMedia}>
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </div>
+            </Tooltip>
+          </div>
+          <div className="w-full p-2">
+            <Tooltip text={ "Clear" } direction={"left"}>
+              <div className="bg-black rounded-full" onClick={onPostClear}>
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </div>
+            </Tooltip>
+          </div>
+        </LeftSection>
+        <MainSection>
+          { addElements.map((element, index) => (
+            <React.Fragment key={index}>
+              {element}
+            </React.Fragment>
+          ))
+          }
+        </MainSection>
+        <RightSection>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+          <div className="w-20 p-2">
+            <div className="bg-black rounded-full">
+              <a href="/">
+                <img className="w-full h-full" src={options} alt="Option buttons" />
+              </a>
+            </div>
+          </div>
+        </RightSection>
       </div>
       <UploadOptions />
     </div>
