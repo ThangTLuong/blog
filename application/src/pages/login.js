c128ab831e8421986ca9c05ab80f74a945b685ad
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2024-02-12 13:47:25 -0800

    Fixed off-centered Login and Registration submit button

--- application/src/pages/login.js
+++ application/src/pages/login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { login } from "../services/login-service";
+import "./styles/login.css";
 
 class Login extends React.Component {
   constructor(props) {
@@ -19,19 +20,40 @@ class Login extends React.Component {
   render() {
     return (
       <div id="body">
-        <form className="form form-center round-edges" action="/" method="POST" onSubmit={ this.submit }>
+        <form
+          className="form form-center round-edges"
+          action="/"
+          method="POST"
+          onSubmit={this.submit}
+        >
           <div className="login-form">
             <div className="form-floating mb-3">
-              <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" 
-              onChange={(data) => this.setState({email: data.target.value})}/>
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+                onChange={(data) => this.setState({ email: data.target.value })}
+              />
               <label htmlFor="floatingInput">Email address</label>
             </div>
             <div className="form-floating">
-              <input type="password" className="form-control" id="floatingPassword" placeholder="Password" 
-              onChange={(data) => this.setState({password: data.target.value})}/>
+              <input
+                type="password"
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+                onChange={(data) =>
+                  this.setState({ password: data.target.value })
+                }
+              />
               <label htmlFor="floatingPassword">Password</label>
             </div>
-            <input className="btn btn-primary center" type="submit" value="Login" />
+            <input
+              className="login-button btn btn-primary"
+              type="submit"
+              value="Login"
+            />
           </div>
         </form>
       </div>
