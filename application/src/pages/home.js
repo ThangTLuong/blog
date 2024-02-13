356016365bfe1a5b4605bb3e2b1ad621e82c02d0
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2024-02-12 17:41:18 -0800

    Added media and text displays to the home page

--- application/src/pages/home.js
+++ application/src/pages/home.js
@@ -1,7 +1,33 @@
 import React from "react";
+import "./styles/home.css";
 
 const Home = () => {
-  return <div id="body"></div>;
-}
+  return (
+    <div id="body">
+      <div className="posts-container">
+        <div className="media-container">
+          {Array.from({ length: 12 }).map((_, index) => (
+            <div className="media-item" key={index}>
+              <div className="item"></div>
+            </div>
+          ))}
+        </div>
+        <div className="button-group">
+          <div className="button">Show more</div>
+        </div>
+        <div className="texts-container">
+          {Array.from({ length: 12 }).map((_, index) => (
+            <div className="texts-item" key={index}>
+              <div className="item"></div>
+            </div>
+          ))}
+        </div>
+        <div className="button-group">
+          <div className="button">Show more</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
