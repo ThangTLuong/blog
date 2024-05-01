ea71f6d2c72d3f806b5596b4f4469f69416d85ba
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2024-05-01 13:25:26 -0700

    Implemented useFetch functionalities

--- application/src/hooks/useFetch.js
+++ application/src/hooks/useFetch.js
@@ -1,1 +1,20 @@
-export default async function useFetch(link) {}
+export default async function useFetch(link, head, body) {
+  const { method } = link;
+  let res;
+
+  if (!method) {
+    return new Error("A method was not declared.");
+  }
+
+  try {
+    if (method === "GET") {
+      res = await fetch(link, head);
+    } else {
+      res = await fetch(link, head, body);
+    }
+  } catch (err) {
+    return new Error(err);
+  }
+
+  return res;
+}
