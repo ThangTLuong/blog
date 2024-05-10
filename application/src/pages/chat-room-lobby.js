719479a523d873182f7de5b2be144c8340b4bea7
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2024-05-09 20:30:44 -0700

    Added slight styling to the lobby

--- application/src/pages/chat-room-lobby.js
+++ application/src/pages/chat-room-lobby.js
@@ -1,11 +1,12 @@
 import React from "react";
 import LeftSection from "../components/left-section";
+import "./styles/chat-room-lobby.css";
 
 export default function Lobby() {
   return (
     <div className="page-container">
       <div className="flex-container">
-        <LeftSection></LeftSection>
+        <div className="lobby-left-section"></div>
         <div className="lobby-display-containter"></div>
       </div>
     </div>
